1d8a2bb91bdfd4ccca46c4b052b36693cfab41ad
lordlycat <lordly_cat@163.com>
2017-10-26 00:26:07 +0800

    完善注册登录

--- JS/login.js
+++ JS/login.js
@@ -4,7 +4,11 @@ var loginBtn = document.querySelector('.login');
 var registerInput = document.querySelector('.registerInput');
 var loginInput = document.querySelector('.loginInput');
 var goToRegister = document.querySelector('.go');
+var welcome = document.querySelector('.welcome');
+var welcomeWords = document.querySelector('.welcomeWords');
+var countTime = document.querySelector('.time');
 
+//跳转到注册
 function goRegister(event) {
     var e = event.target || window.event.target;
 
@@ -12,19 +16,106 @@ function goRegister(event) {
     loginInput.style.display = 'none';
 }
 
+//登录
 function login() {
     var username = inputs[3].value;
-    var password = inputs[4].value;
+    var password = hex_md5(inputs[4].value);
+    if (username.replace(/(^\s*)|(\s*$)/g, '').length === 0) {
+        alert('请输入用户名');
+        return false;
+    }
+
+    //发送请求
+    ajax({
+        url: 'http://127.0.0.1:4999/',
+        method: 'POST',
+        data: {
+            'method': 'login',
+            'username': username,
+            'password': password
+        },
+        success: function(data) {
+            if (data === 'OK') {
+                welcomeWords.innerHTML = '登录成功';
+                welcome.className = 'showWelcome';
+                setTimeout(function() {
+                    var restTime = 4;
+                    var timer = setInterval(function() {
+                        if (restTime < 1) {
+                            restTime = 1;
+                            clearInterval(timer);
+                            window.location.href = 'http://127.0.0.1:4999/HTML/index.html' + '?username=' + username;
+                        }
+                        countTime.innerHTML = restTime;
+                        restTime--;
+                    }, 1000)
+                }, 100);
+            } else if (data === 'failed') {
+                alert('账号或密码错误！');
+            }
+        },
+        error: function(data) {
+            console.log(data);
+        }
+    });
 }
 
+//注册
 function register() {
     var username = inputs[0].value,
         password = inputs[1].value,
         checkPassword = inputs[2].value;
-    if (password !== checkPassword) {
+
+    //输入检测
+    if (username.replace(/(^\s*)|(\s*$)/g, '').length === 0 &&
+        username.replace(/(^\s*)|(\s*$)/g, '').length <= 10) {
+        alert('用户名不能为空且不能超过十个字符');
+        inputs[0].value = '';
+    } else if (password.replace(/(^\s*)|(\s*$)/g, '').length === 0 &&
+        password.replace(/(^\s*)|(\s*$)/g, '').length <= 20) {
+
+        alert('密码不能为空格且不能超过20个字符');
+    } else if (password !== checkPassword) {
         alert('确认密码与密码不一致，请重新输入');
         inputs[1].value = '';
         inputs[2].value = '';
+    } else {
+        //加密
+        password = hex_md5(inputs[1].value);
+
+        //发送请求
+        ajax({
+            url: '/',
+            method: 'POST',
+            data: {
+                'method': 'register',
+                'username': username,
+                'password': password
+            },
+            success: function(data) {
+                if (data === 'OK') {
+                    welcomeWords.innerHTML = '注册成功';
+                    welcome.className = 'showWelcome';
+                    setTimeout(function() {
+                        var restTime = 4;
+                        var timer = setInterval(function() {
+                            if (restTime < 1) {
+                                restTime = 1;
+                                clearInterval(timer);
+                                window.location.href = 'http://127.0.0.1:4999/HTML/index.html' + '?username=' + username;
+                            }
+                            countTime.innerHTML = restTime;
+                            restTime--;
+                        }, 1000)
+                    }, 100);
+                } else if (data === 'failed') {
+                    alert('用户名已存在');
+                }
+            },
+            error: function(data) {
+                console.log(data);
+            }
+        })
     }
 
 }
@@ -32,3 +123,7 @@ function register() {
 goToRegister.addEventListener('click', goRegister, false);
 registerBtn.addEventListener('click', register, false);
 loginBtn.addEventListener('click', login, false);
+
+
+
+//The end
--- app.js
+++ app.js
@@ -1,40 +1,79 @@
-var express = require('express');
-var path = require('path');
-var url = require('body-parser');
-var app = express();
+var express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    crypto = require('crypto'),
+    md5 = crypto.createHash('md5');
+    app = express(),
+    mysql = require('mysql'),
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '88068806',
+        database: 'RPGgame'
+    });
+
+//连接数据库
+connection.connect();
 
 //托管静态资源
 app.use(express.static(path.join(__dirname, '/')));
 
+app.use(bodyParser.raw());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
-app.post('/', function (req, res, nest) {
-    
-})
+app.post('/', function(req, res, nest) {
+    var data = req.body;
 
+    //注册
+    if (data.method === 'register') {
+        var date = new Date(),
+            registerTime = date.toUTCString();
+            
+        //判断用户名是否存在
+        connection.query("SELECT username FROM user WHERE username=" +"\"" +data.username + "\"", function(err, result) {
+            if (result.length !== 0) {
+                res.send('failed');
+            } else {
+                //将用户信息插入数据库
 
+                connection.query("INSERT INTO user SET username=?, password=?, progress=?, registerTime=?",
+                 [data.username, data.password, 0, registerTime], function(err, result) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        res.sendStatus(200);
+                    }
+                });
+            }
+        });
 
+    }
 
+    //登录
+    else if (data.method === 'login') {
+        connection.query('SELECT password FROM user WHERE username=' + data.username, function(err, result) {
+            if (err) {
+                console.log(err);
+            } else if (result.length === 0) {
+                res.send('failed');
+            } else {
+                if (data.password === JSON.parse(JSON.stringify(result))[0].password) {
+                    res.sendStatus(200);
+                } else {
+                    res.send('failed');
+                }
+            }
+        });
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
 
 
 
 //设置监听端口
-app.listen(4999, function () {
+app.listen(4999, function() {
     console.log('Running on port 4999');
 })
