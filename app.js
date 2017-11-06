3ab911776dd24e503b676f1b9c6cd24cab1bae80
lordlycat <lordly_cat@163.com>
2017-11-06 20:28:43 +0800

    final

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.post('/', function(req, res, nest) {
     //注册
     if (data.method === 'register') {
         var date = new Date(),
-            registerTime = date.toUTCString();
+            registerTime = date.toString();
 
         //判断用户名是否存在
         connection.query("SELECT username FROM user WHERE username=" + "\"" + data.username + "\"", function(err, result) {
@@ -40,17 +40,24 @@ app.post('/', function(req, res, nest) {
             } else {
                 //将用户信息插入数据库
 
-                connection.query("INSERT INTO user SET username=?, password=?, registerTime=?", [data.username, data.password, registerTime], function(err, result) {
+                connection.query("INSERT INTO user SET username=?, password=?, registerTime=?", 
+                    [data.username, data.password, registerTime], function(err, result) {
                     if (err) {
                         console.log(err);
                     } else {
-                        connection.query("INSERT INTO gameData SET username=?, progress=?, money=?, level=?, experience=?", [data.username, 0, 0, 1, 0], function(err, result) {
+                        connection.query("INSERT INTO gameData SET username=?, progress=?, money=?, level=?, experience=?", 
+                            [data.username, 0, 0, 1, 0], function(err, result) {
                             if (err) {
                                 console.log(err);
                             } else {
                                 res.sendStatus(200);
                             }
                         });
+
+                        //背包
+                        var goods = ["剑x1", "锻造石x2"];
+                        goods = goods.join(",");
+                        connection.query("INSERT INTO backpack SET username=?, goods=?", [data.username, goods]);
                     }
                 });
             }
@@ -60,7 +67,7 @@ app.post('/', function(req, res, nest) {
 
     //登录
     else if (data.method === 'login') {
-        connection.query("SELECT password FROM user WHERE username=" + data.username, function(err, result) {
+        connection.query("SELECT password FROM user WHERE username=" + "\"" + data.username + "\"", function(err, result) {
             if (err) {
                 console.log(err);
             } else if (result.length === 0) {
@@ -74,14 +81,14 @@ app.post('/', function(req, res, nest) {
             }
         });
     } else if (data.method === 'money') {
-        console.log(data);
-        connection.query("SELECT money FROM gameData WHERE username=" + data.username, function(err, result) {
+        //console.log(data);
+        connection.query("SELECT money FROM gameData WHERE username=" + "\"" + data.username + "\"", function(err, result) {
             var money = parseInt(JSON.parse(JSON.stringify(result))[0].money);
             money += parseInt(data.number);
             if (money < 0) {
                 res.send('notEnough');
             } else {
-                connection.query("UPDATE gameData SET money=" + money + " WHERE username=" + data.username, function(err, result) {
+                connection.query("UPDATE gameData SET money=" + money + " WHERE username=" + "\"" + data.username + "\"", function(err, result) {
                     if (err) {
                         console.log(err);
                     } else {
@@ -91,10 +98,45 @@ app.post('/', function(req, res, nest) {
             }
         });
     } else if (data.method === 'progress') {
-        connection.query("SELECT progress FROM gameData WHERE username=" + data.username, function(err, result) {
+        connection.query("SELECT progress FROM gameData WHERE username=" + "\"" + data.username + "\"", function(err, result) {
             var progress = parseInt(JSON.parse(JSON.stringify(result))[0].progress);
             res.send(progress.toString());
         });
+    } else if (data.method === 'addProgress') {
+        connection.query("SELECT progress FROM gameData WHERE username=" + "\"" + data.username + "\"", function  (err, result) {
+            var progress = parseInt(JSON.parse(JSON.stringify(result))[0].progress);
+            progress++;
+            connection.query("UPDATE gameData SET progress=" + progress + " WHERE username=" + "\"" + data.username + "\"");
+        })
+    } else if (data.method === 'balance') {
+        connection.query("SELECT money FROM gameData WHERE username=" + "\"" + data.username + "\"", function(err, result) {
+            if (err) {
+                console.log(err);
+            } else {
+                var balance = JSON.parse(JSON.stringify(result))[0].money;
+                res.send(balance + '');
+            }
+        })
+    }else if (data.method === 'backpack') {
+        connection.query("SELECT goods FROM backpack WHERE username=" + "\"" + data.username + "\"", function (err, result) {
+            var goods = JSON.parse(JSON.stringify(result))[0].goods;
+            res.send(goods);
+        })
+    }else if (data.method === 'changeBackpack') {
+        connection.query("SELECT goods FROM backpack WHERE username=" + "\"" + data.username + "\"", function (err, result) {
+            var goods = JSON.parse(JSON.stringify(result))[0].goods;
+            goods = goods.split(",");
+            goods.pop;
+            goods.join();
+            connection.query("UPDATE backpack SET goods=" + "\"" + goods[0] + "\"" + " WHERE username=" + "\"" + data.username + "\"", function (err, result) {
+                if (err) {
+                    console.log(err);
+                }else {
+                    res.sendStatus(200);
+                }
+            });
+            
+        })
     }
 });
 
@@ -103,24 +145,24 @@ var ws = new websocketServer({
     port: 5000
 });
 
-var clients = [];//连接池
+var clients = []; //连接池
 
-ws.on('connection', function (ws) {
+ws.on('connection', function(ws) {
     clients.push(ws);
     console.log('A user has connected');
 
-    
+
 
     ws.on('message', function(message) {
         // 广播消息
-        clients.forEach(function(ws){
+        clients.forEach(function(ws) {
             ws.send(message);
         });
     });
 
     ws.on('close', function(message) {
         // 连接关闭时，将其移出连接池
-        clients = clients.filter(function(ws1){
+        clients = clients.filter(function(ws1) {
             return ws1 !== ws;
         })
     });
